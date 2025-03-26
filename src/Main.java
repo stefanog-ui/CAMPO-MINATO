e29a72adcaa0b297a77ac9f4654c64c65345f955
stefanog-ui <giancolastefano58@gmail.com>
2025-03-26 18:55:02 +0100

    creazione finestra
    aggiunta comboBox difficoltà

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    private JFrame frame = new JFrame();
+    private JPanel pannelloPrincipale = new JPanel();
+
+    private void inizializzaFrame() {
+        frame.setSize(1000,1000);
+        frame.setTitle("CAMPO MINATO");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(pannelloPrincipale);
+        frame.setVisible(true);
+    }
+
+    private void inizializzaPannelloPrincipale() {
+        pannelloPrincipale.setLayout(new GridLayout(21, 20));
+    }
+
+
+    private void lanciaGioco() {
+        inizializzaPannelloPrincipale();
+        JComboBox comboBoxDifficolta = new JComboBox();
+        comboBoxDifficolta.addItem("FACILE");
+        comboBoxDifficolta.addItem("MEDIO");
+        comboBoxDifficolta.addItem("DIFFICILE");
+        pannelloPrincipale.add(comboBoxDifficolta);
+        inizializzaFrame();
+    }
+
+
     public static void main(String[] args) {
-            System.out.println("Hello World");
+            Main main = new Main();
+            main.lanciaGioco();
     }
 }
