fbe9a855f749c2c2c4d03fbe829da95c37f50bc0
stefanog-ui <giancolastefano58@gmail.com>
2025-03-30 11:58:23 +0200

    matrice campo in utility

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,14 +1,19 @@
 package utility;
 
+import modello.CampoMinato;
+import modello.Cella;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Utility {
 
     public static void creaCampo(int dimensione,JPanel pannelloPrincipale) {
+        CampoMinato campoM = new CampoMinato(new Cella[dimensione][dimensione]);
         pannelloPrincipale.removeAll();
         pannelloPrincipale.setLayout(new GridLayout(dimensione, dimensione));
         for (int i = 0; i < dimensione * dimensione; i++) {
+            Cella cella = new Cella(i);
             pannelloPrincipale.add(new JButton("Pulsante " + (i + 1)));
         }
         pannelloPrincipale.revalidate();
