df07f589edefe1b115ac42d52f9260e6a2a33cca
stefanog-ui <giancolastefano58@gmail.com>
2025-04-06 10:31:38 +0200

    aggiunta parametro isScoperta

--- src/modello/Cella.java
+++ src/modello/Cella.java
@@ -4,17 +4,20 @@ public class Cella {
     private int id;
     private int numeroMine;
     private boolean hasMina;
+    private boolean isScoperta;
 
     public Cella(int id, int numeroMine, boolean hasMina) {
         this.id = id;
         this.numeroMine = numeroMine;
         this.hasMina = hasMina;
+        this.isScoperta = false;
     }
 
     public Cella(int id) {
         this.id = id;
         numeroMine = 0;
         hasMina = false;
+        isScoperta = false;
     }
 
     public int getId() {
@@ -27,5 +30,13 @@ public class Cella {
 
     public boolean isHasMina() {
         return hasMina;
+    }
+
+    public boolean isScoperta() {
+        return isScoperta;
+    }
+
+    public void setScoperta(boolean scoperta) {
+        isScoperta = scoperta;
     }
 }
