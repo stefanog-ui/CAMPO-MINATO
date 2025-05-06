539aa4bbf4c9e9299f6bdb6468732b5aaae31cfe
stefanog-ui <giancolastefano58@gmail.com>
2025-05-06 18:31:52 +0200

    DEFINITIVO

--- src/vista/Sfondo.java
+++ src/vista/Sfondo.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 public class Sfondo extends JPanel {
     private Image sfondo;
 
-    public Sfondo(String imagePath) {
+    public Sfondo(String percorsoImmagine) {
         try {
-            sfondo = new ImageIcon(Objects.requireNonNull(getClass().getResource(imagePath))).getImage();
+            sfondo = new ImageIcon(Objects.requireNonNull(getClass().getResource(percorsoImmagine))).getImage();
         } catch (Exception e) {
             System.err.println("Errore durante il caricamento dell'immagine di sfondo: " + e.getMessage());
         }
